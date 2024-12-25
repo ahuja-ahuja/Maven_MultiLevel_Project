c2eda507f1672f2a0e9ac9a4d133cd0ced714ff6
Gaurav Ahuja <gaurav.ahuja1991@gmail.com>
2024-12-25 16:08:49 +0000

    maven multi module project with enforce plugin

---
+++ Gaurav_Child1/src/main/java/com/Gaurav/Child1/Util1.java
@@ -0,0 +1,11 @@
+package com.Gaurav.Child1;
+
+public class Util1 {
+    public static void method1(String name){
+        System.out.println("My name is from method1 "+name);
+    }
+
+    public static void method2(String name){
+        System.out.println("My name is from method2 "+name);
+    }
+}
---
+++ Gaurav_child2/src/main/java/UsingCHild1Methods.java
@@ -0,0 +1,8 @@
+import com.Gaurav.Child1.Util1;
+
+public class UsingCHild1Methods {
+    public static void main(String[] args) {
+        Util1.method1("Gaurav");
+        Util1.method2("Ahuja");
+    }
+}
